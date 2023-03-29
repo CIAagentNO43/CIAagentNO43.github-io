9e834c418ded2a829b6651d0a845aa7a14157d5a
Glitch (cosmic-fuzzy-eggplant) <none>
2023-03-29 13:53:37 +0000

    I don't care if it doesn't work. Drop it CUH

---
+++ About.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>I like music</title>
+  <link rel="stylesheet" href="Hurry.css">
+</head>
+<body>
+  <header class="footerified">
+  <nav>
+  <a class="footerified" href="Music-I-made.html">Music I made(none)</a>
+  <a class="footerified" href="New-music.html"> Reviews of New music</a>
+  <a class="footerified" href="Personal classics.html">Obscure music I like</a>
+  </nav>
+  <h1>This site is about music</h1></header>
+      <footer><nav>
+  <a href="Music-I-made.html">Music I made(none)</a>
+  <a href="New-music"> Reviews of New music</a>
+  <a href="Personal classics">Obscure music I like</a>
+  </nav> I like music</footer>
+</body>
+</html>
